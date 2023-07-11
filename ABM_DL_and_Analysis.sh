4344841f7ac5aceebffafb418e8c393a3cf9fda3
Wortax <69532627+Wortax@users.noreply.github.com>
2023-07-11 10:09:43 +0200

    Update ABM_DL_and_Analysis.sh

--- ABM_DL_and_Analysis.sh
+++ ABM_DL_and_Analysis.sh
@@ -1,30 +1,28 @@
-rm Output/transcriptome_result.txt || :
+rm Output/transcriptome_result.txt 2> /dev/null
+rm Temp/transcriptome_result.txt 2> /dev/null
 cd Transcriptome
-rm -rf 	*
+
 while IFS= read -r srr_id; do
     	echo "Downloading: $srr_id"
-	fastq-dump $srr_id
+	fastq-dump --fasta 0 --skip-technical $srr_id
 	echo
-	echo Spliting...
-	split -b 1G $srr_id.fastq "$srr_id"_
-	rm $srr_id.fastq
+	grep -v ">" $srr_id.fasta  | head -n 3
+	echo
 	
-	peptide="$(cut -f4 ../Output/pep_localisation.txt)"
-	echo Searching $peptide in Files :
+	peptide="$(cat ../peptide.txt)"
+	reg_pep="$(python3 ../revtrans.py $peptide)"
+	echo Searching $peptide in $srr_id :
+	result=$(grep -v ">" $srr_id.fasta | grep -E -c $reg_pep );
+	echo $result
 	echo
-	result="0" 
-	for FILE in * ;do echo -$FILE ;result=$(cat $FILE | tr -cd 'ATCG' |LC_ALL=C fgrep -z -o $peptide | grep -c $peptide);
-	a=$(echo $FILE | cut -d/ -f2 | cut -d_ -f1);
-	echo "$a\t$result" >> ../Temp/transcriptome_result.txt;
-	echo; done
-	echo
-	echo
-	rm -rf 	*
+	echo "$srr_id\t$result">> ../Temp/transcriptome_result.txt;
+
+	rm -rf $srr_id.fasta
 done < ../SRR_id/Run_id.txt
 	
 cd ..
-cat Temp/transcriptome_result.txt | awk '{ a[$1]+=$2 }END{ for(i in a) print i,a[i] }' | sort -k 2 -r > Output/transcriptome_result.txt
-rm -rf 	Temp/transcriptome_result.txt || :
+cat Temp/transcriptome_result.txt > Output/transcriptome_result.txt
+cat Output/transcriptome_result.txt
+
 echo "Press enter to close... "
 read temp
-
