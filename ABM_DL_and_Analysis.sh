00616f91be3195e9b7b0dd843af079f805efc414
Wortax <69532627+Wortax@users.noreply.github.com>
2023-07-11 15:45:08 +0200

    Update ABM_DL_and_Analysis.sh

--- ABM_DL_and_Analysis.sh
+++ ABM_DL_and_Analysis.sh
@@ -1,29 +1,59 @@
 rm Output/transcriptome_result.txt 2> /dev/null
 rm Temp/transcriptome_result.txt 2> /dev/null
-cd Transcriptome
+keep=$false
 
+while getopts "hi:r:k" option; do
+    case $option in
+           h) # display Help 
+              echo "Commands :\n-i	Input of the peptide amino acid sequence\n-r	Run IDs list path\n-k	Keep downloaded transcriptomes\n-h	Display this message"
+              return
+              exit;;         
+           \?) # incorrect option
+              echo "Error: Invalid option"
+              return    
+              exit;;          
+           i) peptide=${OPTARG};;
+           r) runlist=${OPTARG};;
+           k) # keep option
+              keep=$true
+              echo "Keeping Downloaded transcriptomes"
+              exit;; 
+    esac
+done
+if [ -z "$peptide" ];
+then echo "no input peptide"
+     return
+fi
+if [ -z "$runlist" ];
+then echo "no Run IDs list"
+     return
+fi
+
+echo "Searching $peptide in Runlist"
 while IFS=, read -r srr_id b; do
 	if [ $srr_id = "Run" ];
 	then continue
 	fi
+	echo
     	echo "Downloading: $srr_id"
-	fastq-dump --fasta 0 --skip-technical $srr_id || continue
+	fastq-dump --fasta 0 --skip-technical --outdir Transcriptome/ $srr_id || continue
 	echo
 	grep -v ">" $srr_id.fasta  | head -n 3
 	echo
 	
-	peptide="$(cat ../peptide.txt)"
+	#peptide="$(cat ../peptide.txt)"
 	reg_pep="$(python3 ../revtrans.py $peptide)"
 	echo Searching $peptide in $srr_id :
-	result=$(grep -v ">" $srr_id.fasta | grep -E -c $reg_pep );
+	result=$(grep -v ">" Transcriptome/$srr_id.fasta | grep -E -c $reg_pep );
 	echo $result
 	echo
-	echo "$srr_id\t$result">> ../Temp/transcriptome_result.txt;
-
-	rm -rf $srr_id.fasta
-done < $(find ../SRR_id -type f -name "*.csv")
+	echo "$srr_id\t$result">> Temp/transcriptome_result.txt;
 	
-cd ..
+	if [ !$keep ]
+	then rm -rf Transcriptome/$srr_id.fasta
+	fi
+done < $runlist
+	
 cat Temp/transcriptome_result.txt > Output/transcriptome_result.txt
 cat Output/transcriptome_result.txt
 
