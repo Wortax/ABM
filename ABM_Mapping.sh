51bacfa80b88ff9c22cddea0a6d2d2a975d2b5f9
Wortax <69532627+Wortax@users.noreply.github.com>
2023-07-13 10:41:27 +0200

    Update ABM_Mapping.sh

--- ABM_Mapping.sh
+++ ABM_Mapping.sh
@@ -19,19 +19,16 @@ fi
 
 
 cd Genome
-rm -rf 	..Output/pep_localisation.txt || :
+rm -rf 	../Output/pep_localisation.txt || :
 
 for FILE in * ;do transeq $FILE ../Temp/$FILE.pep -frame=6; ../search_pep.out -p $peptide; rm ../Temp/*; echo ; done
 echo
 cd ..
 
 peptide_seq="None"
-[ -r Output/pep_localisation.txt ] && cat Output/pep_localisation.txt && peptide_seq="$(cut -f4 Output/pep_localisation.txt)" || echo No Match
+cat Output/pep_localisation.txt || echo No Match
 echo 
 
 
-echo
-echo "Elapsed time: $elapsed s (~"$((elapsed/60))"m) "
-echo
 echo "Press enter to close... "
 read temp
